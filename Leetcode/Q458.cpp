d70923f065489cd5fdd045ebeef63e2c06669030
Fan JIN <kingium@outlook.com>
2020-01-01 22:00:30 -0500

    add Q458: Poor Pigs (encoding view)

---
+++ Leetcode/Q458.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        return ceil(log(buckets) / log(1 + minutesToTest / minutesToDie));
+    }
+};
